3e8d6b5830ce8e59682060e5da53f514f8417527
johoermann <johannes.hoermann@tiffinger-thiel.de>
2021-11-16 14:53:56 +0100

    Add readme and apply changed prettier config

---
+++ README.md
@@ -0,0 +1,13 @@
+# Merge upstream
+
+```
+git checkout upstream/main
+git subtree split -P packages/cra-template-typescript -b temp-branch
+git checkout update
+git merge -s subtree -Xsubtree="packages/cra-template-typescript" temp-branch --allow-unrelated-histories
+```
+
+# Publish
+```
+npm run publish
+```
--- packages/cra-template-typescript/template/src/App.tsx
+++ packages/cra-template-typescript/template/src/App.tsx
@@ -1,9 +1,9 @@
-import React, { FC, ReactNode } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { FC, ReactNode } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 interface Props {
-  headline: ReactNode
+  headline: ReactNode;
 }
 
 const App: FC<Props> = ({ headline }) => {
@@ -12,17 +12,12 @@ const App: FC<Props> = ({ headline }) => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{headline}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
